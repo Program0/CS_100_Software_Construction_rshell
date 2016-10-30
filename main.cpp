631273a1e83c8dbcf9686660b0feb50561c781f1
Program0 <marlzeroth@gmail.com>
2016-10-29 18:20:46 -0700

    Updated the main.cpp sample file

--- main.cpp
+++ main.cpp
@@ -11,25 +11,24 @@ int main(int argc, char *argv[])
     pid_t child_pid;// Process id of the child after forking
     int child_status; // Status of child after fork() executes
 
-    char *a = "ls";
-    char *b[] = {"-la"};
+    char *a[] = {"ls","-la"};
     child_pid = fork();
-    // If we successfully created a child
+    // If we successfully created a child we should execute
+    // commands here
       if(child_pid == 0) {
-        /* We succesfully created a child process. */
           pid_t piChild = waitpid(&child_status);
           if(piChild != child_pid)
 
-        execvp(a, b);
-
-        /* If execvp returns, it must have failed. */
-
-        printf("Unknown command\n");
-        exit(0);
+        execvp(a[0], a);
       }
+      // We failed
+      else if (child_pid < 0){
+          std::cout << "Error: no child process created"<<std::endl;
+      }
+      // The parent process will run here
       else{
+          std::cout<< "Success. We are in the parent."<<std::endl;
 
-          std::cout << "Error: no child waiting"<<std::endl;
       }
 
     Test mytest;
