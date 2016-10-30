3a34238dbf0e4b17534c8542fa258ec69a96cc9f
Program0 <marlzeroth@gmail.com>
2016-10-29 21:17:35 -0700

    added return types to more functions in several classes

--- semicolon_connector.h
+++ semicolon_connector.h
@@ -8,6 +8,6 @@ class Semicolon_Connector : public Connector_B {
     public:
         Semicolon_Connector(Base* left, Base *right);
         virtual ~Semicolon_Connector();
-        execute();
+        int execute();
 };
 #endif
