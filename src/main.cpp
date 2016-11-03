0f8b9511043c146e653072560be2d7a77aad1d7c
Program0 <marlzeroth@gmail.com>
2016-11-02 20:30:11 -0700

    started rshell in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,29 @@
 //Emmilio Segovia esego001@ucr.edu 861305177
 
 // System libraries
+#include <vector>
 
 // User libraries
-#include "test.h"
+#include "tree.h"
+#include "parser.h"
+
 
 int main(int argc, char *argv[])
 {
+
+    Tree commandTree;
+    Parser parser;
+    std::vector< std::vector<std::string> > input;
+
+    while(true){
+
+
+
+    }
+    /*
     Test mytest;
     mytest.testCommand();
     return 0;
-
+    */
 }
 
